c775ec48c4ec9c390a2c75e6c846174a69ac1a1e
nabiya <patannabiya05@gmail.com>
2026-06-30 15:40:41 +0530

    Initial commit

---
+++ Frontend/src/ChatWindow.jsx
@@ -0,0 +1,139 @@
+import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useContext, useState, useEffect } from "react";
+import {ScaleLoader} from "react-spinners";
+import Sidebar from "./Sidebar.jsx";
+
+function ChatWindow() {
+    const {prompt, setPrompt, reply, setReply, currThreadId, setPrevChats, setNewChat,isOpenmenu, setIsOpenmenu} = useContext(MyContext);
+    const [loading, setLoading] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+   
+
+    const getReply = async () => {
+        setLoading(true);
+        setNewChat(false);
+
+        console.log("message ", prompt, " threadId ", currThreadId);
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: prompt,
+                threadId: currThreadId
+            })
+        };
+
+        try {
+            const response = await fetch("http://localhost:8080/api/chat", options);
+            const res = await response.json();
+            console.log(res);
+            setReply(res.reply);
+        } catch(err) {
+            console.log(err);
+        }
+        setLoading(false);
+    }
+
+    //Append new chat to prevChats
+    useEffect(() => {
+        if(prompt && reply) {
+            setPrevChats(prevChats => (
+                [...prevChats, {
+                    role: "user",
+                    content: prompt
+                },{
+                    role: "assistant",
+                    content: reply
+                }]
+            ));
+        }
+
+        setPrompt("");
+    }, [reply]);
+
+
+    const handleProfileClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+         <div className="chatWindow">
+
+        {/* Navbar */}
+        <div className="navbar">
+
+            <div className="menu">
+                <div
+                    className="menuBtn"
+                    onClick={() => setIsOpenmenu(true)}
+                >
+                    <i className="fa-solid fa-bars"></i>
+                </div>
+
+                <span className="logoTitle">
+                    StudyAi <i className="fa-solid fa-chevron-down"></i>
+                </span>
+            </div>
+
+            <div className="userIconDiv" onClick={handleProfileClick}>
+                <span className="userIcon">
+                    <i className="fa-solid fa-user"></i>
+                </span>
+            </div>
+
+        </div>
+
+        {/* Profile Dropdown */}
+        {isOpen && (
+            <div className="dropDown">
+                <div className="dropDownItem">
+                    <i className="fa-solid fa-gear"></i> Settings
+                </div>
+
+                <div className="dropDownItem">
+                    <i className="fa-solid fa-cloud-arrow-up"></i> Upgrade Plan
+                </div>
+
+                <div className="dropDownItem">
+                    <i className="fa-solid fa-arrow-right-from-bracket"></i> Log out
+                </div>
+            </div>
+        )}
+
+        {/* Chat Messages */}
+        <Chat />
+
+        {/* Loading */}
+        <ScaleLoader color="#fff" loading={loading} />
+
+        {/* Input */}
+        <div className="chatInput">
+            <div className="inputBox">
+                <input
+                    type="text"
+                    placeholder="Ask anything..."
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                    onKeyDown={(e) => e.key === "Enter" && getReply()}
+                />
+
+                <div id="submit" onClick={getReply}>
+                    <i className="fa-solid fa-paper-plane"></i>
+                </div>
+                <div><p className="info">
+                StudyAi can make mistakes. Check important info.
+            </p></div>
+            </div>
+
+            
+        </div>
+
+    </div>
+    )
+}
+
+export default ChatWindow;
